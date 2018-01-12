760a5939c351661c72c696af6080f744bc223724
Macronut <Macronut@live.com>
2018-01-12 17:09:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,89 +1,218 @@
 package main
 
 import (
-	"encoding/binary"
+	"bufio"
+	"time"
+	//"encoding/binary"
 	"io"
 	"log"
 	"net"
+	"os"
 	"strconv"
+	"strings"
 
 	"./dns"
 )
 
 type ProxyInfo struct {
-	Method   int
+	Type     uint16
 	AddrList []net.TCPAddr
 	Password string
 }
 
-type DNSInfo struct {
-	Method   int
+type ServerInfo struct {
+	Type     uint16
 	AddrList []net.UDPAddr
-	Proxy    *ProxyInfo
 }
 
-var DNSMap map[string]DNSInfo
-var ProxyMap map[string]ProxyInfo
-var DNSCacheA map[string][]byte
-var DNSCacheAAAA map[string][]byte
-
-func handleLoadConfig(path string) {
-	addrlist := make([]net.UDPAddr, 1)
-	serverAddr, _ := net.ResolveUDPAddr("udp", "8.8.8.8:53")
-	addrlist[0] = *serverAddr
-	DNSMap["\x03www\x06google\x03com\x00"] = DNSInfo{dns.TypeA, addrlist, nil}
-	return
+type DNSCache struct {
+	A      *dns.Answers
+	AAAA   *dns.Answers
+	Server *ServerInfo
+	Proxy  *ProxyInfo
 }
 
-type DNSClient struct {
-	ID   uint16
-	Addr *net.UDPAddr
-}
+var CacheMap map[string]DNSCache
+var ProxyMap map[int32]ProxyInfo
+var DefaultServer ServerInfo
+var ProxyAddress net.TCPAddr
+var NetWorkType int
 
-func handleDNSForward(response []byte, serverAddr *net.UDPAddr, client *net.UDPConn, clientAddr *net.UDPAddr) {
-	srcAddr := &net.UDPAddr{IP: net.IPv4zero, Port: 0}
-	server, err := net.DialUDP("udp", srcAddr, serverAddr)
+func handleLoadConfig(path string) error {
+	conf, err := os.Open(path)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
+	defer conf.Close()
+	br := bufio.NewReader(conf)
+	for {
+		line, _, err := br.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		if line[0] != '#' {
+			config := strings.Split(string(line), "/")
+			configType := strings.Split(config[0], "=")
+			if len(config) > 1 {
+				if configType[0] == "server" {
+					addrlist := make([]net.UDPAddr, 0)
+					if config[2] != "" {
+						for _, addr := range strings.Split(config[2], "|") {
+							if strings.HasPrefix(addr, "[") {
+								if !strings.Contains(addr, "]:") {
+									addr += ":53"
+								}
+							} else {
+								if !strings.Contains(addr, ":") {
+									addr += ":53"
+								}
+							}
+							serverAddr, err := net.ResolveUDPAddr("udp", addr)
+							if err != nil {
+								log.Println(err)
+								continue
+							}
+							addrlist = append(addrlist, *serverAddr)
+						}
+					}
+
+					QName := dns.PackQName(config[1])
+					var AnswerA *dns.Answers = nil
+					var AnswerAAAA *dns.Answers = nil
+					if configType[1] == "A" {
+						AnswerAAAA = &dns.Answers{0, 0, 0, nil}
+					} else if configType[1] == "AAAA" {
+						AnswerA = &dns.Answers{0, 0, 0, nil}
+					}
+					if config[1] == "" {
+						DefaultServer = ServerInfo{dns.UDP, addrlist}
+					} else {
+						CacheMap[string(QName)] = DNSCache{
+							AnswerA,
+							AnswerAAAA,
+							&ServerInfo{dns.UDP, addrlist},
+							nil,
+						}
+					}
+				} else if configType[0] == "address" {
+					AList := []net.IP{}
+					AAAAList := []net.IP{}
+					QName := dns.PackQName(config[1])
+
+					if config[2] != "" {
+						for _, addr := range strings.Split(config[2], "|") {
+							var ip net.IP = net.ParseIP(addr)
+							if strings.Contains(addr, ":") {
+								AAAAList = append(AList, ip)
+							} else {
+								AList = append(AList, ip)
+							}
+						}
+					}
+
+					var AnswerA dns.Answers = dns.PackAnswers(AList)
+					var AnswerAAAA dns.Answers = dns.PackAnswers(AAAAList)
+
+					CacheMap[string(QName)] = DNSCache{
+						&AnswerA,
+						&AnswerAAAA,
+						nil,
+						nil,
+					}
+				}
+			} else {
+				if configType[0] == "server" {
+					addrlist := make([]net.UDPAddr, 0)
+					for _, addr := range strings.Split(configType[1], "|") {
+						addr += ":53"
+						serverAddr, _ := net.ResolveUDPAddr("udp", addr)
+						addrlist = append(addrlist, *serverAddr)
+					}
+					DefaultServer = ServerInfo{dns.UDP, addrlist}
+				} else if configType[0] == "proxy" {
+					pProxyAddress, _ := net.ResolveTCPAddr("tcp", configType[1])
+					ProxyAddress = *pProxyAddress
+				}
+			}
+		}
+	}
+
+	return err
+}
+
+func handleDNSForward(id uint16, server *net.UDPConn, client *net.UDPConn, clientAddr *net.UDPAddr) {
 	defer server.Close()
-	_, err = server.Write(response)
+	//log.Println(response)
 
-	data := make([]byte, 1024)
-	_, _, err = server.ReadFromUDP(data)
-	if err != nil {
-		log.Println(err)
-		return
+	data := make([]byte, 2048)
+	server.SetReadDeadline(time.Now().Add(time.Second * 2))
+	for {
+		//n, _, err := server.ReadFromUDP(data)
+		n, err := server.Read(data)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		header, offset := dns.UnpackHeader(data)
+		id := header.ID
+		question, off := dns.UnpackQuestion(data[offset:])
+		offset += off
+		if header.ID == id {
+			_, err = client.WriteToUDP(data[:n], clientAddr)
+
+			qname := string(question.QName)
+
+			//log.Println(data[:n])
+			cache, ok := CacheMap[qname]
+			answers := dns.Answers{header.ANCount, header.NSCount, header.ARCount, data[offset:n]}
+			if !ok {
+				cache = DNSCache{nil, nil, nil, nil}
+			}
+			if question.QType == dns.TypeA {
+				cache.A = &answers
+				CacheMap[qname] = cache
+			} else if question.QType == dns.TypeAAAA {
+				cache.AAAA = &answers
+				CacheMap[qname] = cache
+			}
+
+			//binary.BigEndian.PutUint16(data, id)
+			break
+		}
+	}
+}
+
+func CacheLookup(QName []byte) (DNSCache, bool) {
+	cache, ok := CacheMap[string(QName)]
+	if ok {
+		//log.Println(string(QName), cache)
+		return cache, ok
+	}
+	offset := int(QName[0]) + 1
+	for i := 0; i < 2; i++ {
+		name := "\x00" + string(QName[offset:])
+		cache, ok = CacheMap[name]
+		if ok {
+			log.Println(name, cache)
+			return cache, ok
+		}
+		o := int(QName[offset])
+		if o == 0 {
+			break
+		}
+		offset += o + 1
 	}
 
-	question, _ := dns.UnpackQuestion(data[12:])
-
-	qname := string(question.QName)
-
-	if question.QType == dns.TypeA {
-		log.Println(question.QType)
-		DNSCacheA[qname] = data
-	} else if question.QType == dns.TypeAAAA {
-		log.Println(question.QType)
-		DNSCacheAAAA[qname] = data
-	}
-
-	//binary.BigEndian.PutUint16(data, id)
-	_, err = client.WriteToUDP(data, clientAddr)
+	return cache, ok
 }
 
 func handleDNSServer(client *net.UDPConn) {
 	data := make([]byte, 512)
-	serverAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
-
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
 	for {
-		_, clientAddr, err := client.ReadFromUDP(data)
+		n, clientAddr, err := client.ReadFromUDP(data)
 		if err != nil {
 			log.Println(err)
 			continue
@@ -91,31 +220,53 @@ func handleDNSServer(client *net.UDPConn) {
 		header, off := dns.UnpackHeader(data)
 		id := header.ID
 		question, off := dns.UnpackQuestion(data[off:])
-		qname := string(question.QName)
-		log.Println(qname)
-		dnsinfo, ok := DNSMap[qname]
+
+		cache, ok := CacheLookup(question.QName)
+		var serverInfo *ServerInfo = nil
+		var answers *dns.Answers = nil
+
 		if ok {
-			log.Println(dnsinfo)
-			if dnsinfo.Method != 255 && int(question.QType) != dnsinfo.Method {
-				client.WriteToUDP(data, clientAddr)
+			if question.QType == dns.TypeA {
+				answers = cache.A
+			} else if question.QType == dns.TypeAAAA {
+				answers = cache.AAAA
 			}
-			serverAddr = &dnsinfo.AddrList[0]
+			//log.Println(cache)
+			if answers != nil {
+				response := dns.PackResponse(data[:n], *answers)
+				client.WriteToUDP(response, clientAddr)
+				//log.Println(response)
+				continue
+			}
+			serverInfo = cache.Server
+		}
+		if serverInfo == nil {
+			if question.QType == dns.TypeAAAA {
+				response := dns.PackResponse(data[:n], dns.Answers{0, 0, 0, nil})
+				client.WriteToUDP(response, clientAddr)
+				continue
+			}
+			serverInfo = &DefaultServer
 		}
 
-		var cache []byte
-		ok = false
-		if question.QType == dns.TypeA {
-			cache, ok = DNSCacheA[qname]
-		} else if question.QType == dns.TypeAAAA {
-			cache, ok = DNSCacheAAAA[qname]
+		//srcAddr := net.UDPAddr{IP: net.IPv4zero, Port: 0}
+		//server, err := net.DialUDP("udp", &srcAddr, &serverAddr)
+		addr, err := net.ResolveUDPAddr("udp", ":0")
+		server, err := net.ListenUDP("udp", addr)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
 
-		if ok {
-			binary.BigEndian.PutUint16(cache, id)
-			client.WriteToUDP(cache, clientAddr)
-		} else {
-			go handleDNSForward(data, serverAddr, client, clientAddr)
+		for _, serverAddr := range (*serverInfo).AddrList {
+
+			_, err = server.WriteToUDP(data[:n], &serverAddr)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
 		}
+		go handleDNSForward(id, server, client, clientAddr)
 	}
 }
 
@@ -161,21 +312,31 @@ func handleProxy(client net.Conn) {
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	//Config
-	DNSMap = make(map[string]DNSInfo)
-	ProxyMap = make(map[string]ProxyInfo)
-	DNSCacheA = make(map[string][]byte)
-	DNSCacheAAAA = make(map[string][]byte)
-	handleLoadConfig("/etc/pino.conf")
+	CacheMap = make(map[string]DNSCache)
+	ProxyMap = make(map[int32]ProxyInfo)
+	NetWorkType = 1
+	//err := handleLoadConfig("/etc/pino.conf")
+	err := handleLoadConfig("pino.conf")
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	//DNS
 	addr, err := net.ResolveUDPAddr("udp", ":53")
 	udpconn, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		log.Panic(err)
+		log.Println(err)
+		addr, err = net.ResolveUDPAddr("udp", ":54")
+		udpconn, err = net.ListenUDP("udp", addr)
+		if err != nil {
+			log.Panic(err)
+			return
+		}
 	}
 	defer udpconn.Close()
 	go handleDNSServer(udpconn)
 	//Proxy
-	l, err := net.Listen("tcp", ":1080")
+	l, err := net.ListenTCP("tcp", &ProxyAddress)
 	if err != nil {
 		log.Panic(err)
 	}
